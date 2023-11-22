40d0912a48ea21bac9755d058460c26211a19eb9
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-11-22 12:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// script.js
+
 function insert_Row() {
-    //Write your code here
   
+    var table = document.getElementById("sampleTable");
+
+    
+    var newRow = table.insertRow(0);
+
   
+    var leftCell = newRow.insertCell(0);
+    
+    leftCell.innerHTML = "New Cell1";
+
+   
+    var rightCell = newRow.insertCell(1);
+    
+    rightCell.innerHTML = "New Cell2";
 }
